b70842aa1e2a564e4c08f663c13c28b7b303a4fa
MrT-Branham <sturner@cuhsd.org>
2024-09-11 14:42:01 -0700

    Added more direction for the interface labs

--- src/interfaces/sort_by_length/WordRunner.java
+++ src/interfaces/sort_by_length/WordRunner.java
@@ -2,8 +2,12 @@ package interfaces.sort_by_length;//(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
 
-import java.io.File; 
+import interfaces.sort_by_vowels.Word;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class WordRunner
@@ -11,13 +15,17 @@ public class WordRunner
 	public static void main( String args[] ) throws IOException
 	{
 		//Scan the file - change with absolute file path
-		Scanner file = new Scanner(new File("words.dat"));
+		Scanner file = new Scanner( new File("C:\\Users\\sturner\\IdeaProjects\\DSA_Practice_Key\\src\\interfaces\\sort_by_vowels\\word.dat") );
+
+		List<interfaces.sort_by_vowels.Word> list = new ArrayList<Word>();
 
 		//Get the file size
 		int size = file.nextInt();
 
 		//TODO: Finish this to collect the data and print the results
-		file.nextLine();
-
+		for( int i = 0; i < size; i++ )
+		{
+			String s = file.next();
+		}
 	}
 }
