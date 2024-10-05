53fb84748e68305c0c98fb3388fbcb96531d4844
MrT-Branham <sturner@cuhsd.org>
2024-10-05 10:34:59 -0700

    Added Phone Number lab.

---
+++ src/maps/PhoneNumber/Phone.java
@@ -0,0 +1,24 @@
+package maps.PhoneNumber;//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.util.*;
+
+public class Phone
+{
+	private Map<String, Integer> m;
+	
+	public Phone()
+	{
+		//complete the constructor				
+	}
+	
+	public String getNumber( String s )
+	{
+		String conv = "";
+		//add in the code to convert
+		//the phone number
+		return conv;
+	}
+
+}
