164e430d2113ad835d37c497d267c0f4bf20f6a3
MrT-Branham <sturner@cuhsd.org>
2024-10-20 10:44:17 -0700

    Added stacks practice

---
+++ src/stacks/make_a_stack/IntStack.java
@@ -0,0 +1,53 @@
+package stacks.make_a_stack;//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+//ArrayList of ints
+//or
+//array of ints
+
+import java.util.ArrayList;
+
+public class IntStack
+{
+	//pick your storage for the stack
+	//you can use the an array or an ArrayList
+
+	//option 1
+	private int[] rayOfInts;
+	private int numInts;
+
+	//option 2
+	private ArrayList<Integer> listOfInts;
+
+	public IntStack()
+	{
+
+	}
+
+	public void push(int item)
+	{
+
+	}
+
+	public int pop()
+	{
+		return 0;
+	}
+
+	public boolean isEmpty()
+	{
+		return false;
+	}
+
+	public int peek()
+	{
+		return 0;
+	}
+
+	public String toString()
+	{
+		return "";
+	}
+}
