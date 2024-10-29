196dafe7eed27a45d36efda86ae64a1d65676dbd
MrT-Branham <sturner@cuhsd.org>
2024-10-29 09:13:42 -0700

    Added queue practice materials

---
+++ src/queues/make_a_queue/IntQueue.java
@@ -0,0 +1,52 @@
+package queues.make_a_queue;//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+//ArrayList of ints
+//or
+//array of ints
+
+import java.util.ArrayList;
+import static java.lang.System.*;
+
+public class IntQueue
+{
+	//pick your storage for the queue
+	//you can use an array or an ArrayList
+
+	//option 1
+	private int[] rayOfInts;
+	private int numInts;
+
+	//option 2
+	private ArrayList<Integer> listOfInts;
+
+	public IntQueue()
+	{
+	}
+
+	public void add(int item)
+	{
+	}
+
+	public int remove()
+	{
+		return 0;
+	}
+
+	public boolean isEmpty()
+	{
+		return false;
+	}
+
+	public int peek()
+	{
+		return 0;
+	}
+
+	public String toString()
+	{
+		return "";
+	}
+}
