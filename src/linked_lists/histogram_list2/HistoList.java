18da4681d310a8f763743563738257bc0e262859
kimmyfanpage123 <111404722+kimmyfanpage123@users.noreply.github.com>
2025-01-15 23:17:44 -0800

    started histogram lab 2 part 2

--- src/linked_lists/histogram_list2/HistoList.java
+++ src/linked_lists/histogram_list2/HistoList.java
@@ -5,37 +5,45 @@ package linked_lists.histogram_list2;//(c) A+ Computer Science
 
 public class HistoList
 {
-	private ListNode front;
+    private ListNode front;
 
-	public HistoList( )
-	{
-	}
+    public HistoList( )
+    {
+        front = null;
+    }
 
 
-	//ADDS NEW NODE TO THE FRONT OF THE LIST FOR LET IF IT DOES NOT EXIST.
-	//IF IT EXISTS, IT BUMPS UP LET'S COUNT BY ONE
-	public void add(Object obj)
-	{
-	}
+    //ADDS NEW NODE TO THE FRONT OF THE LIST FOR LET IF IT DOES NOT EXIST.
+    //IF IT EXISTS, IT BUMPS UP LET'S COUNT BY ONE
+    public void add(Object obj)
+    {
+        Comparable object = (Comparable)obj;
+        if (front == null){
+            front = new ListNode(object, null);
+        }
+        else{
+            front = new ListNode(object, front);
+        }
+    }
 
-	//RETURNS THE INDEX POSITION OF LET IN THE LIST
-	public int indexOf(Object obj)
-	{
-		int spot=-1;
-		return -1;
-	}
+    //RETURNS THE INDEX POSITION OF LET IN THE LIST
+    public int indexOf(Object obj)
+    {
+        int index = 0;
+        
+    }
 
-	//RETURNS A REFERENCE TO THE NODE AT SPOT
-	private ListNode nodeAt(int spot)
-	{
-		ListNode current=front;
-		return current;
-	}
+    //RETURNS A REFERENCE TO THE NODE AT SPOT
+    private ListNode nodeAt(int spot)
+    {
+        ListNode current=front;
+        return current;
+    }
 
-	//RETURNS THE LIST AS A BIG STRING
-	public String toString()
-	{
-		String output = "";
-		return output;
-	}
+    //RETURNS THE LIST AS A BIG STRING
+    public String toString()
+    {
+        String output = "";
+        return output;
+    }
 }
