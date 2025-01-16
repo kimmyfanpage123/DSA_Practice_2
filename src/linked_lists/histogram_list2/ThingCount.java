d1afc3002cd46421140fb464a72a47d8b965071e
kimmyfanpage123 <111404722+kimmyfanpage123@users.noreply.github.com>
2025-01-15 23:12:15 -0800

    finished histogram lab 2 part 1

--- src/linked_lists/histogram_list2/ThingCount.java
+++ src/linked_lists/histogram_list2/ThingCount.java
@@ -5,49 +5,66 @@ package linked_lists.histogram_list2;//(c) A+ Computer Science
 
 public class ThingCount implements Comparable
 {
-	private int count;
-	private Object thing;
-	
-	public ThingCount()
-	{
-	}
-	
-	public ThingCount(Object thang, int cnt)
-	{
-	}
-	
-	public int getCount()
-	{
-		return 0;
-	}
-	
-	public void setCount(int cnt)
-	{
-	}
+    private int count;
+    private Object thing;
 
-	public void setThing(Object obj)
-	{
-	}	
-	
-	public Object getThing()
-	{
-		return null;
-	}
-	
-	public boolean equals(Object obj)
-	{
-		ThingCount other = (ThingCount)obj;
-		return false;
-	}
-	
-	public int compareTo(Object obj)
-	{
-		ThingCount other = (ThingCount)obj;
-		return -1;		
-	}
-	
-	public String toString()
-	{
-		return ""+ getThing() + " - " + getCount();
-	}
+    public ThingCount()
+    {
+        thing = null;
+        count = 0;
+    }
+
+    public ThingCount(Object thang, int cnt)
+    {
+        thing = thang;
+        count = cnt;
+    }
+
+    public int getCount()
+    {
+        return count;
+    }
+
+    public void setCount(int cnt)
+    {
+        count = cnt;
+    }
+
+    public void setThing(Object obj)
+    {
+        thing = obj;
+    }
+
+    public Object getThing()
+    {
+        return thing;
+    }
+
+    public boolean equals(Object obj)
+    {
+        ThingCount other = (ThingCount)obj;
+        if (thing.equals(other.getThing()) && count == other.getCount()){
+            return true;
+        }
+        return false;
+    }
+
+    public int compareTo(Object obj)
+    {
+        ThingCount other = (ThingCount)obj;
+        if (count < other.getCount()){
+            return -1;
+        }
+        else if (count == other.getCount()){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+
+    public String toString()
+    {
+        return ""+ getThing() + " - " + getCount();
+    }
 }
