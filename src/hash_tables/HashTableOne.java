c7d5a47ed338ad07b75c90b693a82b174b625d25
MrT-Branham <sturner@cuhsd.org>
2025-02-10 18:43:42 -0800

    Added Hash Table files

---
+++ src/hash_tables/HashTableOne.java
@@ -0,0 +1,33 @@
+package hash_tables;//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//hashtable example 1
+
+public class HashTableOne
+{
+	public static void main ( String[] args )
+	{
+		Object[] hashTable = new Object[10];
+		
+		Character c = new Character('1');
+		hashTable[c.hashCode()%10] = c;
+		
+		Integer num = new Integer(113);		//boxing up the int the long way
+		hashTable[num.hashCode()%10] = num;
+		
+		String s = "e";		
+		hashTable[s.hashCode()%10] = s;
+		
+		//add more objects to the hashtable
+		
+		
+		
+		
+		
+			
+		for( Object thing : hashTable )
+		{
+			System.out.println(thing);
+		}   	   
+	}
+}
