34f4510dd9f35702ab31a9284505d06dea0343a9
MrT-Branham <sturner@cuhsd.org>
2025-02-22 10:23:46 -0800

    Updated runners and added packages for heaps practice

--- src/heaps/heap_sort/HeapSortRunner.java
+++ src/heaps/heap_sort/HeapSortRunner.java
@@ -1,4 +1,4 @@
-//(c) A+ Computer Science
+package heaps.heap_sort;//(c) A+ Computer Science
 //www.apluscompsci.com
 
 public class HeapSortRunner
@@ -8,9 +8,17 @@ public class HeapSortRunner
 		HeapSort heap = new HeapSort();
 
 		int[] nums = {99,2,8,75,10,7,9,17,5,3,4,1,11,1};
-		
+
 		heap.heapSort(nums);
-		
+
+		System.out.println(heap);
+
+		heap = new HeapSort();
+
+		int[] stuff = {-3,28,18,5,3,17,29,6,5,3,4,1,11,1};
+
+		heap.heapSort(stuff);
+
 		System.out.println(heap);
 	}
 }
